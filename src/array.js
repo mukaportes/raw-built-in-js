3644541a55c32661ad0a2070315da8306dfbcb05
Murilo Portescheller <murilo.portescheller@avanade.com>
2019-11-25 23:13:17 -0300

    Add array.filter() method

--- src/array.js
+++ src/array.js
@@ -38,17 +38,35 @@ module.exports = function (inputArray = []) {
   this.reduce = function (accumulatorFn = () => { }, accumulator) {
     let accumulatorCopy;
 
+    // NOTE: used arguments so I wouldnt need to know the 2nd arg data type to create its clone
     ([fnCopy, accumulatorCopy] = [...arguments]);
     const arrayLength = inputArray.length;
 
     for (let counter = 0; counter < arrayLength; counter += 1) {
       const item = inputArray[counter];
 
-      if (accumulatorFn.length === 2) accumulatorCopy = accumulatorFn(accumulatorCopy, item);
-      if (accumulatorFn.length === 3) accumulatorCopy = accumulatorFn(accumulatorCopy, item, counter);
-      if (accumulatorFn.length === 3) accumulatorCopy = accumulatorFn(accumulatorCopy, item, counter, inputArray);
+      if (accumulatorFn.length === 2) {
+        accumulatorCopy = accumulatorFn(accumulatorCopy, item);
+      } else if (accumulatorFn.length === 3) {
+        accumulatorCopy = accumulatorFn(accumulatorCopy, item, counter);
+      } else if (accumulatorFn.length === 4) {
+        accumulatorCopy = accumulatorFn(accumulatorCopy, item, counter, inputArray);
+      }
     }
 
     return accumulatorCopy;
   }
+
+  this.filter = function(filterFn = () => true) {
+    const filteredArray = [];
+    const arrayLength = inputArray.length;
+
+    for (let counter = 0; counter < arrayLength; counter += 1) {
+      const item = inputArray[counter];
+
+      if (filterFn(item)) filteredArray.push(item);
+    }
+
+    return filteredArray;
+  };
 }
